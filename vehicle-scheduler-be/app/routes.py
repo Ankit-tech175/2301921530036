6a5e60c785d036a6bd79f282bbffaced19a3f15e
Ankit <ankitchauhan19092005@gmail.com>
2026-06-26 11:54:10 +0530

    Implemented vehicle scheduler backend structure and API integration

--- vehicle-scheduler-be/app/routes.py
+++ vehicle-scheduler-be/app/routes.py
@@ -1,4 +1,5 @@
 from flask import Blueprint, jsonify
+from app.services import get_depots, get_vehicles
 
 main = Blueprint("main", __name__)
 
@@ -8,3 +9,25 @@ def home():
     return jsonify({
         "message": "Vehicle Scheduler Backend Running"
     })
+
+
+@main.route("/depots")
+def depots():
+    try:
+        data = get_depots()
+        return jsonify(data)
+    except Exception as e:
+        return jsonify({
+            "error": str(e)
+        }), 500
+
+
+@main.route("/vehicles")
+def vehicles():
+    try:
+        data = get_vehicles()
+        return jsonify(data)
+    except Exception as e:
+        return jsonify({
+            "error": str(e)
+        }), 500
--- vehicle-scheduler-be/app/services.py
+++ vehicle-scheduler-be/app/services.py
@@ -1,25 +1,29 @@
 import requests
 from app.config import DEPOT_API, VEHICLE_API, ACCESS_TOKEN
 
-
-headers = {
-    "Authorization": f"Bearer {ACCESS_TOKEN}"
+HEADERS = {
+    "Authorization": f"Bearer {ACCESS_TOKEN}",
+    "Content-Type": "application/json"
 }
 
 
 def get_depots():
     response = requests.get(
         DEPOT_API,
-        headers=headers
+        headers=HEADERS,
+        timeout=10
     )
 
+    response.raise_for_status()
     return response.json()
 
 
 def get_vehicles():
     response = requests.get(
         VEHICLE_API,
-        headers=headers
+        headers=HEADERS,
+        timeout=10
     )
 
+    response.raise_for_status()
     return response.json()
